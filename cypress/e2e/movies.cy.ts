eb51a6ee45d386f9a7fd8dca8be00ffbeb9a6633
Eli Ennab <elinaennab@gmail.com>
2023-03-29 10:20:35 +0200

    two successful options on test that get at least 10 movies when searching for the matrix

--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -23,8 +23,16 @@ describe('Mostly Mundane Movies', () => {
 				.contains('Search query must be at least 3 characters long, duh ^^ 🙄')
 		})
 
-		it.skip('should be able to search for the The Matrix and find X number of movies', () => {
+		it('should be able to search for the The Matrix and get at least 10 movies', () => {
+			cy.get('.form-control')
+				.type(`The Matrix{enter}`)
 
+			cy.get(':nth-child(10)')
+				.should('exist')
+
+			cy.get('.py-3')
+				.find(':nth-child(10)')
+				.should('exist')
 		})
 
 		it.skip('should show a loading spinner while searching for a movie', () => {
