0483f113d21dc6921f377596b0e6aa8c44c96aed
Eli Ennab <elinaennab@gmail.com>
2023-03-29 10:04:47 +0200

    structure for all "happy path" tests

--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -1,5 +1,29 @@
 describe('Mostly Mundane Movies', () => {
-  it('should be able to visit the page', () => {
-    cy.visit('/')
-  })
+
+	context('initial state', () => {
+		beforeEach(() => {
+			cy.visit('/')
+		})
+
+		it.skip('should not be able to search for movies without a title', () => {
+
+		})
+
+		it.skip('should not be able to search without a title less than 3 chars', () => {
+
+		})
+
+		it.skip('should be able to search for the The Matrix and find X number of movies', () => {
+
+		})
+
+		it.skip('should show a loading spinner while searching for a movie', () => {
+
+		})
+
+		it.skip('should be able to click on the first movie after a successful search and the page you are directed to should match the ID of the movie', () => {
+
+		})
+
+	})
 })
